21f145188c760e8231ae2a3203a7c6f81c7e5626
allenlcp <allen.myconcordia@gmail.com>
2019-08-11 12:17:42 -0400

    First commit

---
+++ Notes.md
@@ -0,0 +1,5 @@
+### Performance calculation
+Time complexity
+Memory complexity
+
+Use worse case better than taking average
---
+++ datastruc_algo/src/main/java/com/example/datastruc_algo/DatastrucAlgoApplication.java
@@ -0,0 +1,18 @@
+package com.example.datastruc_algo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastrucAlgoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastrucAlgoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+	}
+}
