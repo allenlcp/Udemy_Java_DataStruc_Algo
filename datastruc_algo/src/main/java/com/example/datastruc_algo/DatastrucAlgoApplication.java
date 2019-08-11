55b61a187f88067a6432e2c5ae985955b14b1580
allenlcp <allen.decodemtl@gmail.com>
2019-08-11 12:19:42 -0400

    Fine tuning setup

--- datastruc_algo/src/main/java/com/example/datastruc_algo/DatastrucAlgoApplication.java
+++ datastruc_algo/src/main/java/com/example/datastruc_algo/DatastrucAlgoApplication.java
@@ -13,6 +13,6 @@ public class DatastrucAlgoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+
 	}
 }
