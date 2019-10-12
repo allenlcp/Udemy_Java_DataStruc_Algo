934bcae48c5640cae3b757fa9ef68d82c96ab1ef
allenlcp <allen.mygithub@gmail.com>
2019-10-12 12:28:56 -0400

    RadixSort - stable counting sort

--- datastruc_algo/src/main/java/com/example/datastruc_algo/_02_sort_algo/SortAlgoMain.java
+++ datastruc_algo/src/main/java/com/example/datastruc_algo/_02_sort_algo/SortAlgoMain.java
@@ -23,7 +23,10 @@ public class SortAlgoMain implements CommandLineRunner {
 //        runSelectionSort();
 //        runInsertionSort();
 //        runShellSort();
-        runMergeSort();
+//        runMergeSort();
+//        runQuickSort();
+//        runCountingSort();
+        runRadixSort();
 
         // Factorial
         // 1! = 1 * 0! = 1
@@ -197,6 +200,84 @@ public class SortAlgoMain implements CommandLineRunner {
 
 	    return factorial;
     }
+
+    private void runQuickSort() {
+        int[] intArray = {20 ,35, -15, 7, 55, 1, -22};
+
+        quickSort(intArray, 0, intArray.length);
+
+        System.out.println("Quicksort -> " + Arrays.toString(intArray));
+    }
+
+    public void quickSort(int[] input, int start, int end){
+	    if(end-start < 2){
+	        return;
+        }
+
+	    int pivotIndex = quickSortPartition(input, start, end);
+	    quickSort(input, start, pivotIndex);
+	    quickSort(input, pivotIndex+1, end);
+    }
+
+    public int quickSortPartition(int[] input, int start, int end){
+
+	    // This is using the first element as the pivot
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while(i<j){
+            // NOTE: empty loop body
+            while(i < j && input[--j] >= pivot);
+
+            if(i<j){
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while(i<j && input[++i] <= pivot);
+
+            if(i<j){
+                input[j] = input[i];
+            }
+        }
+
+        input[j] = pivot;
+        return j;
+    }
+
+    private void runCountingSort() {
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+
+        countingSort(intArray, 0, intArray.length);
+
+        System.out.println("CountingSort -> " + Arrays.toString(intArray));
+    }
+
+    private void countingSort(int[] input, int min, int max){
+	    int[] countArray = new int[(max-min) + 1];
+
+	    for(int i=0; i< input.length; i++){
+	        countArray[input[i] - min]++;
+        }
+
+	    int j = 0;
+	    for(int i=min; i<= max; i++){
+	        while(countArray[i-min] > 0){
+	            input[j++] = i;
+	            countArray[i-min]--;
+            }
+        }
+    }
+
+    private void runRadixSort() {
+        int[] intArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+
+        countingSort(intArray, 0, intArray.length);
+
+        System.out.println("CountingSort -> " + Arrays.toString(intArray));
+    }
+
 
     private int runRecursiveFactorial(int num){
         if(num == 0){ // known as the break case or default condition (required in recursion, otherwise we will get a stackoverflow error)
